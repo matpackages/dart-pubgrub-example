319eb4f902f00ec6760669b25495a12f8e614a9b
Patrick <patrsc@nubu.at>
2023-02-25 23:26:53 +0100

    Changes in elm.dart

--- elm.dart
+++ elm.dart
@@ -1,3 +1,15 @@
+import 'util.dart';
+import 'versions.dart';
+
 void main() async {
-    print('Hello');
+    var packageFile = 'universes/elm/elm-packages.json';
+    var packages = readTest(packageFile);
+    var server = await startPackageServer(packages);
+    var name = 'AdrianRibao_elm_derberos_date';
+    var version = '1.0.0';
+    var root = packages[name][version];
+    print('${name}/${version}:');
+    await solveVersions('cache', 'root', parseConstraints(root), server.url);
+    print('');
+    await server.close();
 }
